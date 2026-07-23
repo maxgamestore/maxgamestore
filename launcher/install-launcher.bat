5375453145d7f30ca6cbee38ac24d927ac6d71c7
maxgamestore <maxgamestore5@gmail.com>
2026-07-23 11:06:34 +0300

    Update install-launcher.bat

--- launcher/install-launcher.bat
+++ launcher/install-launcher.bat
@@ -9,8 +9,8 @@ REM Crează folderul de instalare
 set INSTALL_DIR=%USERPROFILE%\Desktop\WorldOfFights
 if not exist "%INSTALL_DIR%" mkdir "%INSTALL_DIR%"
 
-REM Copiază Launcherul în folder
-copy "%~dp0WorldOfFights_Launcher.jar" "%INSTALL_DIR%\"
+REM Descarcă Launcherul de pe GitHub
+curl -L -o "%INSTALL_DIR%\WorldOfFights_Launcher.jar" "https://raw.githubusercontent.com/maxgamestore/maxgamestore/main/launcher/WorldOfFights_Launcher.jar"
 
 REM Crează shortcut pe Desktop
 powershell -Command "$WS = New-Object -ComObject WScript.Shell; $SC = $WS.CreateShortcut('%USERPROFILE%\Desktop\World Of Fights.lnk'); $SC.TargetPath = 'javaw'; $SC.Arguments = '-jar \"%INSTALL_DIR%\WorldOfFights_Launcher.jar\"'; $SC.WorkingDirectory = '%INSTALL_DIR%'; $SC.Save()"
@@ -20,7 +20,6 @@ echo ========================================
 echo   INSTALARE FINALIZATA!
 echo ========================================
 echo.
-echo Jocul a fost instalat in: %INSTALL_DIR%
 echo Pe Desktop a aparut shortcut-ul "World Of Fights"
 echo.
 pause
